fd8887d1be8ea1144e3fe892009380362d9495d2
Shijitha Jenifer J <shijithajenifer2005@gmail.com>
2026-09-17 20:39:36 +0530

    Time: 4 ms (100%), Space: 44.4 MB (99.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) 
+        return false;
+        int rev = 0,digit,copyx = x;
+        while(x!=0){
+            digit = x%10;
+            rev = rev*10+digit;
+            x = x/10;
+        }
+        return rev == copyx;
+    }
+}
